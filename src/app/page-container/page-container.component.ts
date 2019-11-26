20a12c53713093a3565f8fed628bfd9a54d2405e
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-11-26 21:41:40 +0530

    init

---
+++ src/app/page-container/page-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-container',
+  templateUrl: './page-container.component.html',
+  styleUrls: ['./page-container.component.css']
+})
+export class PageContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
